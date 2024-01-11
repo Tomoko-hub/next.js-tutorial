d76c7f4db6831e0ede0f11ab540cc7d9c7880c76
Tomoko-hub <haisaitomoco@gmail.com>
2024-01-11 20:51:03 +0200

    first commit

---
+++ pages/500.js
@@ -0,0 +1,9 @@
+import utilStyles from "../styles/utils.module.css";
+
+export default function Custom505(){
+    return(
+        <div>
+            <h1 className={utilStyles.heading2Xl}>Can not found page.</h1>
+        </div>
+    );
+}
